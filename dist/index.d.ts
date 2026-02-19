51187491494e1f00825f869f85b3e5d02c9f6f08
Mishael <themishaeldaap@gmail.com>
2026-02-19 08:16:57 +0100

    Ran gitbuild to update dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,3 @@
-import { TranspilerConfig } from "./config/transpilerConfig";
-import { ValidationResult } from "./validator/types/validationResult";
-declare class SalesforceMetadataTranspiler {
-    private readonly validator;
-    private readonly config;
-    constructor(config: TranspilerConfig);
-    transpile(input: unknown): Promise<ValidationResult<any>>;
-}
-export { SalesforceMetadataTranspiler };
+export { SalesforceMetadataTranspiler } from "./salesforceMetadataTranspiler";
+export { PackageBuilder } from "./packageBuilder/PackageBuilder";
+//# sourceMappingURL=index.d.ts.map
